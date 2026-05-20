66f51a7fcde3ed66e7f836ddb976b7c3b66accab
Pedro Henrique <38799159+pedrophs2@users.noreply.github.com>
2026-05-20 17:10:17 -0300

    Add express to application

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import client from './client';
 import { Steps } from './steps';
 import 'dotenv/config';
+import express from 'express';
 
 client.on('message', new Steps().handleMessage.bind(new Steps()));
 
 client.initialize();
+
+const port = process.env.PORT || 4000 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
